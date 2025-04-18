9fbd704cb3c8266ca442712f6a1a00895adbb726
Robaire Galliath <galliath.robaire@gmail.com>
2025-04-17 23:23:47 -0400

    Add GetCameraTrajectory

--- third_party/ORB_SLAM3/include/System.h
+++ third_party/ORB_SLAM3/include/System.h
@@ -140,6 +140,9 @@ public:
     void Shutdown();
     bool isShutDown();
 
+    // Extract camera trajectory from the system
+    std::vector<Eigen::Matrix4f> GetCameraTrajectory();
+
     // Save camera trajectory in the TUM RGB-D dataset format.
     // Only for stereo and RGB-D. This method does not work for monocular.
     // Call first Shutdown()
