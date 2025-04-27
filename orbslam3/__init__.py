7f3edf43582041859f91395d8a30fa3866952ef6
Robaire Galliath <galliath.robaire@gmail.com>
2025-04-27 19:26:50 -0400

    Update exports

--- orbslam3/__init__.py
+++ orbslam3/__init__.py
@@ -1,3 +1,3 @@
-from .orbslam3 import system, TrackingState, Sensor, IMUPoint
+from .orbslam3 import system, TrackingState, Sensor
 
-__all__ = ["system", "TrackingState", "Sensor", "IMUPoint"]
+__all__ = ["system", "TrackingState", "Sensor"]
