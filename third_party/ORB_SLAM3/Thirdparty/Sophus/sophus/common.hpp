c95c6d8602f22a1262653ab7edd306509bc52b98
Robaire Galliath <galliath.robaire@gmail.com>
2025-04-29 08:04:43 -0400

    Disable SOPHUS_ENSURE

--- third_party/ORB_SLAM3/Thirdparty/Sophus/sophus/common.hpp
+++ third_party/ORB_SLAM3/Thirdparty/Sophus/sophus/common.hpp
@@ -12,9 +12,13 @@
 
 #include <Eigen/Core>
 
+// Disable SOPHUS_ENSUREs to prevent orbslam from crashing
+#define SOPHUS_DISABLE_ENSURES
+#include "formatstring.hpp"  // Keep this to week orbslam compiling
+
 #if !defined(SOPHUS_DISABLE_ENSURES)
 #include "formatstring.hpp"
-#endif //!defined(SOPHUS_DISABLE_ENSURES)
+#endif  //! defined(SOPHUS_DISABLE_ENSURES)
 
 // following boost's assert.hpp
 #undef SOPHUS_ENSURE
@@ -37,8 +41,8 @@
 #define EIGEN_DEVICE_FUNC
 #endif
 
-// NVCC on windows has issues with defaulting the Map specialization constructors, so
-// special case that specific platform case.
+// NVCC on windows has issues with defaulting the Map specialization
+// constructors, so special case that specific platform case.
 #if defined(_MSC_VER) && defined(__CUDACC__)
 #define SOPHUS_WINDOW_NVCC_FALLBACK
 #endif
